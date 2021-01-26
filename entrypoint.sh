e2a42518144ddb9db1663a00f7ad10304016615f
Bastien Roy <bastien@edgegap.com>
2021-01-26 17:54:55 -0500

    custom html page

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 
 python3 /app/context_proxy.py
 cp /app/config.cfg /etc/nginx/sites-enabled/default
+cp /app/index.html /var/www/html/index.nginx-debian.html
 service nginx stop
 /usr/sbin/nginx -g "daemon off;"
